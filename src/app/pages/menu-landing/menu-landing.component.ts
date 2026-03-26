f26f4f368566019558e605b5982604ef2ce24b49
MiguelDev1 <loormiguel40@gmail.com>
2026-03-25 20:52:20 -0500

    Normalize line endings

--- src/app/pages/menu-landing/menu-landing.component.ts
+++ src/app/pages/menu-landing/menu-landing.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from "../../components/navbar/navbar.component";
+// import { FooterComponent } from "../../components/footer/footer.component";
+// import { FloatingHomeButtonComponent } from "../../components/floating-home-button/floating-home-button.component";
 
 @Component({
   selector: 'app-menu-landing',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent /*, FooterComponent, FloatingHomeButtonComponent*/],
   templateUrl: './menu-landing.component.html',
   styleUrls: ['./menu-landing.component.css']
 })
