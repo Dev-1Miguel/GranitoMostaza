4776c3f68fa0f474a9b0558b7fc1ce2cd60de014
MiguelDev1 <loormiguel40@gmail.com>
2026-04-02 21:39:30 -0500

    (feat) floating
    - Se implemento botones flotantes en paginas de login, new account
    - Se implemente el boton flotante de compras cuando me movilizo por la pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { CartMainSection } from './sections/shopping-cart-section/cart-main-section';
 import { CartService } from './services/cart.service';
+import { FloatingCartButtonComponent } from './components/floating-cart-button/floating-cart-button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, CartMainSection],
+  imports: [RouterOutlet, CommonModule, CartMainSection, FloatingCartButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
